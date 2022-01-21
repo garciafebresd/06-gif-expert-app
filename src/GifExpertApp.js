580ce908ccdeb10658783f1aba9b0963ef500f26
garciafebresd <garciafebresd@gmail.com>
2022-01-21 15:04:50 -0300

    components GifGrid and GifGridItem added

--- src/GifExpertApp.js
+++ src/GifExpertApp.js
@@ -1,36 +1,29 @@
-import React, {useState} from 'react';
-import { AddCategory } from './components/AddCategory';
+import React, { useState } from "react";
+import { AddCategory } from "./components/AddCategory";
+import { GifGrid } from "./components/GifGrid";
 
 export const GifExpertApp = () => {
+  const [categories, setCategories] = useState(["One Punch"]);
 
-    const [categories, setCategories] = useState(
-            [
-                'One Punch', 
-                'Samurai X', 
-                'Dragon Ball'
-            ]
-        );
-    
-    // const handleAdd = () => {
-    //     setCategories([...categories, 'HunterXHunter']);
-    // }
+  return (
+    <>
+      <h2>GifExpertApp</h2>
 
-    return (
-        <>
-         <h2>GifExpertApp</h2>
+      <AddCategory setCategories={setCategories} />
 
-         <AddCategory setCategories={setCategories} />
-
-         <hr />
-         <ol>
-           {
-               categories.map((category, i) => {
-                    return <li key={category}>{category}</li>
-               })
-           } 
-         </ol>
-        </>
-    );
-}
+      <hr />
+      <ol>
+        {
+            categories.map((category, i) => (
+                <GifGrid 
+                    key={category}
+                    category={category}
+                />
+            ))
+        }
+      </ol>
+    </>
+  );
+};
 
 export default GifExpertApp;
